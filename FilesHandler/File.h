5427dc42c6d642c9c515816c3df9bb560df13599
David Urbina <duParra15@gmail.com>
2019-06-13 23:24:09 -0400

    Fixed errors.
    Fixed object to char sequence function.
    Fixed char sequence write method.

--- FilesHandler/File.h
+++ FilesHandler/File.h
@@ -6,6 +6,7 @@
 #include <cstring>
 
 using namespace std;
+
 /**
  * @author David Urbina.
  * Helper template to handle operations over fstream files.
@@ -17,28 +18,28 @@ class File {
         File(char *name) { init(name, NULL, NULL); }
 
         /**
-         * Contructor for setting pointers to transformation functions which transforms 
+         * Contructor for setting pointers to transformation functions which transforms
          * from string to type T instances and viceversa.
          * @param name the name of the file.
          * @param (*toObject) pointer of function that takes a char sequence as argument and returns
          * an instance of the type T composed from the argument.
          * @param (*toString) pointer of function that takes an instance of the type T as argument
-         * and return the char sequence representation of the argument. 
+         * and return the char sequence representation of the argument.
          */
-        File(char *name, T(*toObject)(char *str), char*(*toString)(T Obj) ) { init(name, toObject, toString); }
+        File(char *name, T(*toObject)(char *str), void(*toString)(T Obj, char *buffer) ) { init(name, toObject, toString); }
         ~File(){}
 
         /**
          * Opens the fstream with the given open mode flags.
          * This method involves a verification to determine whether the name is valid.
          * @param flags the flags for opening the fstream.
-         */ 
+         */
         void open(const ios_base::openmode &flags);
         void close(){ file.close(); }
 
         //Reading and writing for text files.
         void read(char* dest)   { file.getline(dest, 300, '\n'); }
-        void write(char *source){ file.write(source, 300); }
+        void write(char *source){ file<<source<<endl; }
 
         //Reading and writing for binary files.
         T read(){
@@ -58,7 +59,7 @@ class File {
         void gotoSOF(bool read = true){ // Go to the start of the file.
             realocate(0, read);
 
-            //Clear the error flags triggered by the previous file stream.
+             //Clear the raised fstream flags.
             file.clear();
         }
 
@@ -68,7 +69,9 @@ class File {
         T getBuffer()   { return buffer;      }
         int getLength() { return length;      }
 
-        //Compute the length of the file, in lines.
+        /*
+         * Compute the length of the file, in lines.
+         */
         int computeLength(){ return file.tellg()/sizeof(T); }
 
         void setName(char* name){ strcpy(this->name, name); }
@@ -77,8 +80,7 @@ class File {
         void asObject(char* str){ buffer = toObject(str);  }
 
         //FIXME: function returning thrash.
-        char* asString(T buffer){ return toString(buffer); }
-        char* asString(){ return asString(buffer); }
+        void asString(char *dest){ toString(buffer, dest); }
 
         void print();
 
@@ -92,7 +94,7 @@ class File {
 
         //Transformation function pointers.
         T(*toObject)(char *str);
-        char*(*toString)(T obj);
+        void(*toString)(T Obj, char *buffer);
 
         /**
          * Initialization method to avoid code duplication, called in constructors.
@@ -100,14 +102,13 @@ class File {
          * @param (*toObject) pointer of function that takes a char sequence as argument and returns
          * an instance of the type T composed from the argument.
          * @param (*toString) pointer of function that takes an instance of the type T as argument
-         * and return the char sequence representation of the argument. 
+         * and return the char sequence representation of the argument.
          */
-        void init(char* name, T(*toObject)(char *str), char*(*toString)(T obj) );
+        void init(char* name, T(*toObject)(char *str), void(*toString)(T Obj, char *buffer) );
 };
 
-
 template <typename T>
-void File<T>::init(char* name, T(*toObject)(char *str), char*(*toString)(T obj) ){
+void File<T>::init(char* name, T(*toObject)(char *str), void(*toString)(T Obj, char *buffer) ){
     strcpy(this->name, name);
     this->toObject = toObject;
     this->toString = toString;
