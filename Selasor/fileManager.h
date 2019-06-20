2797b2408c41838db8a911b25c70a2afb459cd7b
Carlos Rosales <carlosros033@gmail.com>
2019-06-19 22:20:13 -0400

    Fixing some issues.

--- Selasor/fileManager.h
+++ Selasor/fileManager.h
@@ -61,7 +61,7 @@ class fileManager
     void read(char *line_)                                  { char line[300]; file.getline(line, 300, '\n'); strcpy(line_, line); }
     void read(T &b)                                         { file.read(reinterpret_cast<char *>(&b), size); }
     void readAndConvert(T &b, T(*convertion)(char *str))    { char line[300]; file.getline(line, 300, '\n'); fileBuffer = convertion(line);}
-    void write(char *line_)                                 { strcat(*line_, "\n"); file << line_; }
+    void write(char *line_)                                 { char line[300]; strcpy(line,line_); strcat(line, "\n"); file << line; }
     void write(T &b)                                        { file.write(reinterpret_cast<char *>(&b), size); }
     void readFrom(int pos)                                  { file.seekg(pos*size, ios::beg); }
     void writeFrom(int pos)                                 { file.seekp(pos*size, ios::beg); }
@@ -69,7 +69,7 @@ class fileManager
     int getWritePos()                                       { return (file.tellp() / size); }
     int getSize(bool filetype)                              { if(filetype == BIN_FILE) { int previousPos = getReadPos(); file.seekg(0, ios::end);
                                                                 int sizeOfFile = file.tellg() / size; readFrom(previousPos); return sizeOfFile; } 
-                                                              else { int previousPos = getReadPos(); file.seekg(0, ios::end); int i = 0;
+                                                              else { int previousPos = getReadPos(); file.seekg(0, ios::beg); int i = 0;
                                                                 while (!file.eof()){char temp[300]; read(temp); if(strcmp(temp, "") != 0) i++;}
                                                                 readFrom(previousPos); return i; } }
     bool getEOF()                                           { return file.eof(); }
