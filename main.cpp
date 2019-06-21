404f7e50bc4fea0cb791d78e44c3519df3a91cc5
eagskunst(WSL) <emmanuelnypd@gmail.com>
2019-06-20 21:11:50 -0400

    Added support to block search

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "FilesHandler\Archivo.h"
 #include "Custom Classes\Persona.h"
+#include "Searching/busquedas.h"
 using namespace std;
 
 int main(int argc, char const *argv[]){
@@ -30,5 +31,19 @@ int main(int argc, char const *argv[]){
         cout<<personasFile2.read()<<endl;
     }
     personasFile2.cerrar();
+    Persona v[] = {Persona("Emmanuel", 19),
+                   Persona("Johan", 19),
+                   Persona("Gabriel", 19),
+                   Persona("Carlos", 19),
+                   Persona("Rafael", 19),
+                   Persona("Andrea", 19),
+                   Persona("Angelica", 19),
+                   Persona("Klisman", 19),
+                   Persona("Maria Daniela", 19),
+                   Persona("Bertha", 19)};
+    int n = sizeof(v)/sizeof(Persona);
+    cout<<"Pos: "<<busquedaPorBloques(v,n,Persona("Bertha", 19))<<endl;
+    cout<<"Pos: "<<busquedaPorBloques(v,n,Persona("Ivan", 19))<<endl;
+
     return 0;
 }
