b8bbe6ddcee798de0f36c936eb97f21fc87ece2c
eagskunst(WSL) <emmanuelnypd@gmail.com>
2019-06-29 00:16:38 -0400

    Create Node template class

---
+++ Collections/Node.h
@@ -0,0 +1,44 @@
+#ifndef NODE_H
+#define NODE_H
+
+template <typename T>
+class Node{
+private:
+    T mData;
+    Node<T> prev;
+    Node<T> next;
+
+public:
+    Node(T data);
+    T getData();
+    Node<T> *getPrev();
+    Node<T> *getNext();
+    void setData(T data);
+};
+
+template <typename T>
+Node<T>::Node(T data):prev(NULL),next(NULL){
+    this->mData = data;
+}
+
+template <typename T>
+void Node<T>::setData(T data){
+    this->mData = data;
+}
+
+template <typename T>
+T Node<T>::getData(){
+    return this->mData;
+}
+
+template <typename T>
+Node<T>* Node<T>::getPrev(){
+    return this->prev;
+}
+
+template <typename T>
+Node<T>* Node<T>::getNext(){
+    return this->next;
+}
+
+#endif
