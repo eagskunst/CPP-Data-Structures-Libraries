8b74caf16d7e1d9bb8970a0bb782f1febca47905
David Urbina <duParra15@gmail.com>
2019-07-21 12:36:17 -0400

    Created working version template of this collection.

--- Collections/LinkedPile.h
+++ Collections/LinkedPile.h
@@ -3,11 +3,42 @@
 
 #include "Collections/LinkedBase.h"
 
-template <typename T>
-class LinkedPile: LinkedBase<T>{
+template <class T>
+class LinkedPile: public LinkedBase<T>{
     public:
-        LinkedPile(): LinkedBase<T>(){};
-        LinkedPile(T data): LinkedBase<T>(data){};
+        LinkedPile(): LinkedBase<T>(){}
+        LinkedPile(T data): LinkedBase<T>(data){}
+
+        int add(const T data){ return LinkedBase<T>::pushBack(data); }
+        int remove(T&);
+        void clear(){ LinkedBase<T>::clear(); }
+
+    private:
+        //Grant acces over parent's class attributes.
+        using LinkedBase<T>::head;
+        using LinkedBase<T>::tail;
+        using LinkedBase<T>::length;
 };
+
+template <typename T>
+int LinkedPile<T>::remove(T &buffer){
+    if(length == 0) return -1;
+
+    if(length == 1){
+        buffer = head->getData();
+        this->clear();
+
+        LinkedBase<T>::linkSentinels();
+    }else{
+        //Unlink the tail node from the pile.
+        tail->prev()->setNext(head);
+        head->setPrev(tail->prev());
+
+        buffer = tail->getData();
+        tail = tail->prev();
+    }
+
+    return length--;
+}
 
 #endif // LINKEDPILE_H
