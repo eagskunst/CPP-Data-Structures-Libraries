64a58c7443e8d3b3f8ffa113c9c65c3425a181af
eagskunst(WSL) <emmanuelnypd@gmail.com>
2019-07-31 10:51:33 -0400

    Metodo de eliminacion para DoubleLinkedList

--- Collections/eagskunst/DoubleLinkedList.h
+++ Collections/eagskunst/DoubleLinkedList.h
@@ -2,6 +2,8 @@
 #define DOUBLE_LINKED_LIST_H
 #include "BaseStructure.h"
 
+using namespace std;
+
 template <typename T>
 class DoubleLinkedList : public BaseStructure<T>{
     private:
@@ -77,8 +79,66 @@ bool DoubleLinkedList<T>::insertAtEnd(T data){
     this->tail->next = nNode;
     this->tail = nNode;
     this->size++;
+    return true;   
+}
+
+template <typename T>
+bool DoubleLinkedList<T>::deleteAtEnd(){
+    if(isEmpty()) return false;
+    if(this->size == 1){
+        delete this->head;
+    }
+    else{
+        Node<T>* currentTail = this->tail;
+        Node<T>* mRef = currentTail->prev;
+        mRef->next = NULL;
+        this->tail = mRef;
+        delete currentTail;
+    }
+    this->size--;
     return true;
-    
+}
+
+template <typename T>
+bool DoubleLinkedList<T>::deleteAtStart(){
+    if(isEmpty()) return false;
+    if(this->size == 1){
+        delete this->head;
+    }
+    else{
+        Node<T>* currentHead = this->head;
+        Node<T>* mRef = currentHead->next;
+        mRef->prev = NULL;
+        this->head = mRef;
+        delete currentHead;
+    }
+    this->size--;
+    return true;
+}
+
+template <typename T>
+bool DoubleLinkedList<T>::deleteElement(T data){
+    if(isEmpty()) return false;
+    Node<T>* mHead = this->head;
+    Node<T>* mTail = this->tail;
+    if(mHead != NULL && mHead->getData() == data){
+        delete mHead;
+        return true;
+    }
+    else if(mTail != NULL && mTail->getData() == data){
+        delete mTail;
+        return true;
+    }
+    else if(mHead != NULL && mTail != NULL){
+        Node<T>* checkNode = mHead->next;
+        Node<T>* prevCheckNode = checkNode->prev;
+        while(checkNode != mTail){
+            if(checkNode->getData() == data) {
+                prevCheckNode->next = checkNode->next;
+                checkNode->prev = prevCheckNode;
+            }
+        }
+    }
 }
 
 template <typename T>
